7bc51226872bce0f8a8252e321ae8d69b40461b7
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-04-25 15:01:28 +0100

    Fetch and display messages with useSWR

--- --0 useSWR/useSWR/src/Chat.jsx
+++ --0 useSWR/useSWR/src/Chat.jsx
@@ -1,17 +1,22 @@
 import useSWR from 'swr'
 
-function Chat() {
-  const MESSAGES_API = '/api/messages';
+const MESSAGES_API = '/api/messages';
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+}
 
-  const fetcher = async (url) => {
-    const response = await fetch(url);
-    return response.json();
-  }
+function Chat() {
+
+  const {data:messages, error} = useSWR(MESSAGES_API, fetcher);
+  if(error) return <div>Error: {error.message}</div>
 
   return (
     <div>
       <ul>
-        {/* render list of messages here */}
+        {messages.map((message)=>{
+          <li key={meaage.id}>{message.text}</li>
+        })}
       </ul>
       <form>
         {/* render form to submit new messages here */}
