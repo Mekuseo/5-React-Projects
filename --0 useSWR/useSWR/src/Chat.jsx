bfde2222dcdb887bc1f4df41e1968d40be0c62b9
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-04-25 15:09:42 +0100

    Create a form to submit new messages

--- --0 useSWR/useSWR/src/Chat.jsx
+++ --0 useSWR/useSWR/src/Chat.jsx
@@ -1,28 +1,54 @@
-import useSWR from 'swr'
+import useSWR, { mutate } from "swr";
 
-const MESSAGES_API = '/api/messages';
+const MESSAGES_API = "/api/messages";
 const fetcher = async (url) => {
   const response = await fetch(url);
   return response.json();
-}
+};
 
 function Chat() {
+  const { data: messages, error } = useSWR(MESSAGES_API, fetcher);
+  if (error) return <div>Error: {error.message}</div>;
 
-  const {data:messages, error} = useSWR(MESSAGES_API, fetcher);
-  if(error) return <div>Error: {error.message}</div>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const text = formData.get("text");
+
+    const response = await fetch(MESSAGES_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+
+    if (response.ok) {
+      event.target.reset();
+      mutate(MESSAGES_API);
+    } else {
+      console.error("Error creating Message;", response.status);
+    }
+  };
+
+  if (error) {
+    return <div>Error loading messages</div>;
+  }
 
   return (
     <div>
       <ul>
-        {messages.map((message)=>{
-          <li key={meaage.id}>{message.text}</li>
+        {messages.map((message) => {
+          <li key={message.id}>{message.text}</li>;
         })}
       </ul>
-      <form>
-        {/* render form to submit new messages here */}
+      <form onSubmit={handleSubmit}>
+        <input name="text" required />
+        <button type="submit">Send</button>
       </form>
     </div>
-  )
+  );
 }
 
 export default Chat;
